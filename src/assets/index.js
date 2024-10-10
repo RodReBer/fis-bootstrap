c29c3445480b0d6de7e8d766181d5ca7b8c54d44
RodReBer <rodrigorey2005@gmail.com>
2024-10-10 00:55:38 -0300

    photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import bootstrap from '/src/assets/bootstrap.png'; 
+import bootstrap from '/src/assets/bootstrap.webp'; 
 import img2 from '/src/assets/img2.webp';
 import ing from '/src/assets/ing.jpg';
 import ort from '/src/assets/ort.jpg';
